c0f25cf9ad1e47325f97fa3eb7e17ac559b7d029
Diante Dowdell <diante.dowdell99@gmail.com>
2023-01-27 18:29:35 -0600

    adding file structure for channel list and channel container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+impor
+
+const apiKey = 'khx9nvqvecgz';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Chat Application</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team dark'>
+        <ChannelListContainer />
+        <ChannelContainer  />
+      </Chat>
     </div>
   )
 }
