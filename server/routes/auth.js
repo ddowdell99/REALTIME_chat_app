348e324348701473abef6b0092553f79de9c5e77
Diante Dowdell <diante.dowdell99@gmail.com>
2023-02-07 21:22:40 -0600

    created back end auth route to receive front end client information and create new accounts through get stream

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const expres = require('express');
+
+const { signup, login } = require('../controllers/auth.js')
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+
+module.export = router;
