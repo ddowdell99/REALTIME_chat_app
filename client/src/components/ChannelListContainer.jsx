800df420767dbc8f72aa4dafa73b535a82b429d2
Diante Dowdell <diante.dowdell99@gmail.com>
2023-02-20 23:03:49 -0600

    redoing and committing recent work for creating channel

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -35,7 +35,7 @@ const CompanyHeader = () => {
 }
 
 
-const ChannelListContainer = () => {
+const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsEditing }) => {
   const logout = () => {
       cookies.remove("token");
       cookies.remove('userID');
@@ -63,6 +63,10 @@ const ChannelListContainer = () => {
             <TeamChannelList
               {...listProps}
               type='team'
+              isCreating = {isCreating} 
+              setIsCreating = {setIsCreating}
+              setCreateType = {setCreateType}
+              setIsEditing = {setIsEditing}
             />
           )}
           Preview = {(previewProps) => (
@@ -81,6 +85,10 @@ const ChannelListContainer = () => {
             <TeamChannelList
               {...listProps}
               type='messaging'
+              isCreating = {isCreating} 
+              setIsCreating = {setIsCreating}
+              setCreateType = {setCreateType}
+              setIsEditing = {setIsEditing}
             />
           )}
           Preview = {(previewProps) => (
